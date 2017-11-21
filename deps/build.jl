f84aa4c2a79642e12b0018c4f23de543e931939c
aokeson <Alexandra.Okeson@colorado.edu>
2017-11-21 01:23:10 -0800

    Update build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -4,15 +4,15 @@ using BinDeps
 
 xgboost = library_dependency("xgboost", aliases = ["libxgboost.so"])
 
-if haskey(ENV, "XGBOOST_BUILD_VERSION") && ENV["XGBOOST_BUILD_VERSION"] == "master"
-    libcheckout = `git checkout master`
-    onload = "global const build_version = \"master\""
-    info("Using the latest master version of the XGBoost library")
-else
-    libcheckout = `git checkout v0.60`
-    onload = "global const build_version = \"0.60\""
-    info("Using the latest stable version (0.60) of the XGBoost library")
-end
+#if haskey(ENV, "XGBOOST_BUILD_VERSION") && ENV["XGBOOST_BUILD_VERSION"] == "master"
+libcheckout = `git checkout master`
+onload = "global const build_version = \"master\""
+info("Using the latest master version of the XGBoost library")
+#else
+#    libcheckout = `git checkout v0.60`
+#    onload = "global const build_version = \"0.60\""
+#    info("Using the latest stable version (0.60) of the XGBoost library")
+#end
 
 provides(BuildProcess,
     FileRule(joinpath(BinDeps.libdir(xgboost), "libxgboost.so"),
